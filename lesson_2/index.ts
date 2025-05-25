2275d9fdc9ae268af8f46c31f2566519533297c7
Ziyodullo <ziyodillo808@gmail.com>
2025-05-26 01:52:08 +0500

    lesson 2 finished

---
+++ lesson_2/index.ts
@@ -0,0 +1,20 @@
+// Data types
+
+let a = 12;
+
+// number
+let ab: number;
+ab = 12;
+
+// string
+let str: string;
+str = "string";
+
+// boolean
+let boo: boolean;
+boo = true;
+
+// null
+let n: null = null;
+
+let un: undefined = undefined;
